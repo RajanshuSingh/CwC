00c00be82c687bfcd00e996e7fa4dc27122eaae6
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-05 09:23:29 +0530

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+//number
+var num = 10;
+
+//string
+
+var str = "Hello World";
+
+//boolean
+
+var bool = true;
+
+//undefined
+
+var und = undefined;
+
+//null
+
+var nul = null;
+
+//bigint
+
+var bigInt = 1234567890123456789012345678901234567890n;
+
+
+console.log(typeof(bool));
+console.log(typeof(str));
+
+
+
+
+
