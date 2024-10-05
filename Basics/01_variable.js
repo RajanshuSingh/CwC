5d99f27eed4716001492bac2adef5abf73dfc258
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-05 09:23:08 +0530

    variables

---
+++ Basics/01_variable.js
@@ -0,0 +1,23 @@
+
+const accountId=14453
+let accountEmail="rajanshusingh258"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId=3
+accountEmail="shubhsingh258"
+accountPassword="6969"
+accountCity="bhopal"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table({accountId, accountEmail, accountPassword, accountCity,accountState});
+
+
+
+
+
