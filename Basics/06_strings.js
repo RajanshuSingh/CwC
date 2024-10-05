98cb8edecb152ad82b20ee9cde35216173853559
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-05 12:33:39 +0530

    strings

---
+++ Basics/06_strings.js
@@ -0,0 +1,16 @@
+
+
+const name="hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)  //modern way to write and widely used especiaaly in backend.
+
+const gameName= new String('rajanshusingh')
+
+console.log(gameName.toUpperCase())
+console.log(gameName.substring(0,5))
+console.log(gameName.slice(2,-9))
+
+
