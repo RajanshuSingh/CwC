15a0d9e11039d88a76c09d395be656cfff0d0e1b
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-06 19:47:13 +0530

    changed folder names

--- 02_Basics/tempCodeRunnerFile.js
+++ 02_Basics/tempCodeRunnerFile.js
@@ -1,2 +1,42 @@
-const Arr2= newArray(1,2,3,4,5)
-// console.log(myArr[0]); 
+//singleton---> only one instance of the object is created(when we declare as consructor)
+// //factory---> multiple instances of the object is created(when we declare as lirerals)
+
+// //object literals
+
+// const mySym= Symbol("key1")
+
+
+
+// const JsUser={
+//     name:'John',
+//     age:30 ,
+//     location:'India',
+//    [ mySym]:"key1",
+//     email: "rajanshusignh258",
+//     isLoggedin: true,
+//     LastLoginDays: ["Monday","Tuesday","Wednesday"]
+
+
+
+// }
+
+// // myArray=[1,2,3,4,5]
+// // console.log(myArray[3])
+
+// // console.log(JsUser.name)
+// // console.log(JsUser['isLoggedin'])  //try to use this one only
+// // console.log(typeof JsUser[mySym])
+
+// JsUser.name="Rajanshu"
+// JsUser['email']="rajanshusingh259"
+// console.log(JsUser.name)
+// console.log(JsUser['email'])
+// // Object.freeze(JsUser)
+
+// // console.log(JsUser)
+
+// JsUser.email = "rajanshusingh6969@gmail.com"
+// JsUser.name="Singh"
+
+// console.log(JsUser)
+// // console.log(JsUser['name'])
