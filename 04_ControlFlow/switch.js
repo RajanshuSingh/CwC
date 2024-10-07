6dbc6a4b3985a0ac864705771367386c1ec05d52
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-07 13:54:34 +0530

    switch and break statement

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,44 @@
+// switch(key){
+//     case value1:
+//         break;
+
+//         default:
+//             break;
+// } 
+
+const month= 10;
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3 :
+        console.log("March")
+        break;
+    case 4:
+        console.log("January")
+        break;
+    case 5:
+        console.log("January")
+        break;
+    case 6:
+        console.log("January")
+        break;
+    case 7:
+        console.log("January")
+        break;
+    case 8:
+        console.log("January")
+        break;
+    case 9:
+        console.log("January")
+        break;
+
+    default:
+        console.log(`no matches have been made`)
+        break;
+}
+// if no break statement is used, all the cases after the matching case will be executed except the default case.
+
