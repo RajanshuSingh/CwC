4ccfe5d3213daf0e096f9738836a866b9375c2dc
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-07 21:23:59 +0530

    maps

---
+++ 05_Iterations/maps.js
@@ -0,0 +1,9 @@
+const myNumbers =[1,2,3,4,5,6,7,8,9,10];
+
+myNumbers.map((num)=>{
+    // console.log(num+10)
+})
+
+const newNums= myNumbers.map((num)=>{ return num*10 }).map((num)=>{return num+1}).filter((num)=>num>50)
+
+console.log(newNums)
