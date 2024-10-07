b671844cb4766cc299fce3611204f69a5f18ffc2
Rajanshu Singh <rajanshusingh258@gmail.com>
2024-10-07 16:04:25 +0530

    while and dowhile loop

---
+++ 05_Iterations/while.js
@@ -0,0 +1,19 @@
+
+// while (condition) {
+//     // code to run if condition is true
+    
+// }
+let i=0;
+while (i<=10) {
+    // code to run if condition is true
+    console.log(`Value of i is ${i}`);
+    i+=2;
+    
+}
+
+// do while loop- code will run atleast once, pehle kaam hoga baad me condition check hoga
+ let score=11;
+do {
+    console.log(`Value of i is ${score}`);
+    score+=1
+} while (score<=10);
